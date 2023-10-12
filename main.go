1cbd1bb2ae6c017ded9d1fcf38bed840b2c3a0ca
Ernesto Rodriguez <ernesto.antonio.rod@gmail.com>
2023-10-12 06:59:31 -0500

    Add Router type and ServeHTTP method

--- main.go
+++ main.go
@@ -15,7 +15,23 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:ernesto@mail.me\">ernesto@mail.me</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		// TODO: handle the page not found error
+// 		// w.WriteHeader(http.StatusNotFound)
+// 		// fmt.Fprint(w, "Page not found")
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -28,10 +44,11 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
 }
+
 func main() {
-	http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/", homeHandler)
 	// http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
